03a0e9f559b174562d49fc0b7d265f172ad9b50a
Łukasz Madej <lukaszmad95@gmail.com>
2018-08-15 14:02:06 +0000

    changed entity name to single

--- src/main/java/com/barterownia/model/VideoGame.java
+++ src/main/java/com/barterownia/model/VideoGame.java
@@ -10,7 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity (name= "video_games")
+@Entity (name= "video_game")
 @Getter
 @Setter
 @AllArgsConstructor
