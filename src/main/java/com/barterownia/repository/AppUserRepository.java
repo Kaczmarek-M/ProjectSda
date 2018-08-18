8403a0b0a230247521f57946119477b17d2036a4
lukmad1 <lukaszmad95@gmail.com>
2018-08-18 10:44:43 +0200

    basic AppUserService functionality

---
+++ src/main/java/com/barterownia/repository/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.barterownia.repository;
+
+import com.barterownia.model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+
+    Optional<AppUser> findAppUserByUsername(String username);
+
+}
