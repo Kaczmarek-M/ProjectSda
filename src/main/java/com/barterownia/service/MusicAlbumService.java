105d14e50c5797f0a3d54c59dc8c2eadabcaaaf1
lukmad1 <lukaszmad95@gmail.com>
2018-08-22 17:44:45 +0200

    -mapping for single auction

--- src/main/java/com/barterownia/service/MusicAlbumService.java
+++ src/main/java/com/barterownia/service/MusicAlbumService.java
@@ -1,6 +1,7 @@
 package com.barterownia.service;
 
 import com.barterownia.model.MusicAlbum;
+import com.barterownia.model.VideoGame;
 import com.barterownia.repository.MusicAlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,49 +16,53 @@ public class MusicAlbumService {
     @Autowired
     private MusicAlbumRepository musicAlbumRepository;
 
-    public void addAlbum(MusicAlbum musicAlbum){
+    public void addAlbum(MusicAlbum musicAlbum) {
         musicAlbumRepository.save(musicAlbum);
     }
 
-    public List<MusicAlbum> getAllAlbums(){
+    public List<MusicAlbum> getAllAlbums() {
         return musicAlbumRepository.findAll();
     }
 
-    public void deleteAlbum(Long id){
+    public void deleteAlbum(Long id) {
         musicAlbumRepository.deleteById(id);
     }
 
-    public List<MusicAlbum> findByBandName(String bandName){
+    public List<MusicAlbum> findByBandName(String bandName) {
         return musicAlbumRepository.findAllByBandNameContaining(bandName);
     }
 
-    public List<MusicAlbum> findByAlbumName(String albumName){
+    public List<MusicAlbum> findByAlbumName(String albumName) {
         return musicAlbumRepository.findAllByAlbumNameContaining(albumName);
     }
 
-    public List<MusicAlbum> findByCarrier (String carrier){
+    public Optional<MusicAlbum> findById(long id) {
+        return musicAlbumRepository.findById(id);
+    }
+
+    public List<MusicAlbum> findByCarrier(String carrier) {
         return musicAlbumRepository.findAllByCarrier(carrier);
     }
 
-    public void editAlbum (MusicAlbum musicAlbum){
+    public void editAlbum(MusicAlbum musicAlbum) {
         Optional<MusicAlbum> optionalMusicAlbum = musicAlbumRepository.findById(musicAlbum.getId());
 
-        if(optionalMusicAlbum.isPresent()){
+        if (optionalMusicAlbum.isPresent()) {
             MusicAlbum editMusicAlbum = optionalMusicAlbum.get();
 
-            if(musicAlbum.getAlbumName() != null){
+            if (musicAlbum.getAlbumName() != null) {
                 editMusicAlbum.setAlbumName(musicAlbum.getAlbumName());
             }
 
-            if(musicAlbum.getBandName() != null){
+            if (musicAlbum.getBandName() != null) {
                 editMusicAlbum.setBandName(musicAlbum.getBandName());
             }
 
-            if(musicAlbum.getCarrier() != null){
+            if (musicAlbum.getCarrier() != null) {
                 editMusicAlbum.setCarrier(musicAlbum.getCarrier());
             }
 
-            if(musicAlbum.getPublicationDate() != null){
+            if (musicAlbum.getPublicationDate() != null) {
                 editMusicAlbum.setPublicationDate(musicAlbum.getPublicationDate());
             }
 
@@ -65,7 +70,7 @@ public class MusicAlbumService {
         }
     }
 
-    public List<MusicAlbum> findByPublicationDateBetweenTwoDates(LocalDate min, LocalDate max){
+    public List<MusicAlbum> findByPublicationDateBetweenTwoDates(LocalDate min, LocalDate max) {
         return musicAlbumRepository.findAllByPublicationDateIsBetween(min, max);
     }
 
