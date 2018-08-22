d183b7688f7ae4d58bc15562f22cd4a83c7734c1
lukmad1 <lukaszmad95@gmail.com>
2018-08-22 17:44:45 +0200

    -mapping for single auction

--- src/main/java/com/barterownia/controller/AuctionController.java
+++ src/main/java/com/barterownia/controller/AuctionController.java
@@ -1,7 +1,7 @@
 package com.barterownia.controller;
 
 import com.barterownia.model.Auction;
-import com.barterownia.service.AuctionService;
+import com.barterownia.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/auction")
@@ -18,8 +19,20 @@ public class AuctionController {
     @Autowired
     private AuctionService auctionService;
 
+    @Autowired
+    private LaptopService laptopService;
+
+    @Autowired
+    private MobilePhoneService mobilePhoneService;
+
+    @Autowired
+    private MusicAlbumService musicAlbumService;
+
+    @Autowired
+    private VideoGameService videoGameService;
+
     @GetMapping(path = "/list/{category}")
-    public String getAuctionList(Model model, @PathVariable(name = "category")String category){
+    public String getAuctionList(Model model, @PathVariable(name = "category") String category) {
         List<Auction> byCategory = auctionService.findByCategory(category);
 
         model.addAttribute("auctionList", byCategory);
@@ -27,6 +40,35 @@ public class AuctionController {
         byCategory.forEach(System.out::print);
 
         return "/auctionList";
+    }
+
+    @GetMapping(path = "/{id}")
+    public String getAuction(Model model, @PathVariable(name = "id") long id) {
+
+        Optional<Auction> auctionOptional = auctionService.findById(id);
+
+        if (!auctionOptional.isPresent()) {
+            return "/home";
+        }
+
+        Auction auction = auctionOptional.get();
+
+        model.addAttribute("auction", auction);
+
+        String tableName = auction.getItem().getCategory().getTableName();
+        Long itemId = auction.getItem().getItemId();
+
+        if ("mobile_phone".equals(tableName)){
+            model.addAttribute("item",mobilePhoneService.findById(itemId));
+        }else if ("laptop".equals(tableName)){
+            model.addAttribute("item",laptopService.findById(itemId));
+        }else if ("music_album".equals(tableName)){
+            model.addAttribute("item",musicAlbumService.findById(itemId));
+        }else if ("video_game".equals(tableName)){
+            model.addAttribute("item",videoGameService.findById(itemId));
+        }
+
+        return "/auction";
     }
 
 }
