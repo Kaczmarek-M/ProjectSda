b3067d3d19b46066d3bd8b68f823b4cec9b09c82
lukmad1 <lukaszmad95@gmail.com>
2018-08-22 18:27:33 +0200

    Merge branch 'FAQ' of https://bitbucket.org/lukmad1/barterownia into lista_aukcji
    # Conflicts:
    #	src/main/java/com/barterownia/controller/IndexController.java
    #	src/main/resources/application.properties

---
+++ src/main/java/com/barterownia/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.barterownia.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(path = "/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @RequestMapping(path = "/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping(path = "/statutes")
+    public String statutes() {
+        return "rules";
+    }
+
+    @GetMapping(path = "/")
+    public String home() {
+        return "/mainPage";
+    }
+
+
+}
