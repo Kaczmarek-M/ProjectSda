4b432f703035bb06d7ad194dd088b8ad33d9367a
lukmad1 <lukaszmad95@gmail.com>
2018-08-22 07:15:53 +0200

    -auction list template -changed field to Object types in some model classes -AuctionController class + auction list mapping

--- src/main/java/com/barterownia/model/Item.java
+++ src/main/java/com/barterownia/model/Item.java
@@ -16,7 +16,7 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private long itemId;
+    private Long itemId;
 
     @ManyToOne
     private Category category;
