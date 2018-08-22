49631cecb0e5c681987d318aacb023225d8c407d
Dominik <dominik-nowak@hotmail.com>
2018-08-22 18:28:44 +0200

    -footer always on bottom -added searchbar in header -added log in button -added modal with log in panel to log in button -added carousel to mainPaige -radio buttons in sidebar changed to standard linked buttons -and a few cosmetics changes

--- src/main/java/com/barterownia/repository/MobilePhoneRepository.java
+++ src/main/java/com/barterownia/repository/MobilePhoneRepository.java
@@ -2,7 +2,6 @@ package com.barterownia.repository;
 
 import com.barterownia.model.MobilePhone;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
